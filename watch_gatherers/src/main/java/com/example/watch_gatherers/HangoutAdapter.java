85c2dfc044cbbfd1810d9f5ab57d677282e3bc50
Khan, Uzma Sharmila <ukhan48@gatech.edu>
2025-04-18 20:59:55 -0400

    added rsvp button functionality to watch

--- watch_gatherers/src/main/java/com/example/watch_gatherers/HangoutAdapter.java
+++ watch_gatherers/src/main/java/com/example/watch_gatherers/HangoutAdapter.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
@@ -23,6 +28,7 @@ public class HangoutAdapter extends RecyclerView.Adapter<HangoutAdapter.HangoutV
     private List<Hangout> hangouts;
     private boolean showGroupName;
     private Map<String, String> groupIdToName = new HashMap<>();
+
 
     public HangoutAdapter(Context context, List<Hangout> hangouts) {
         this(context, hangouts, false);
@@ -50,7 +56,26 @@ public class HangoutAdapter extends RecyclerView.Adapter<HangoutAdapter.HangoutV
     @Override
     public void onBindViewHolder(@NonNull HangoutViewHolder holder, int position) {
         Hangout hangout = hangouts.get(position);
-
+        String me = "RWmRbh838Tf1u0u8BsatsqFj1m02";
+        holder.rsvpToggle.setOnCheckedChangeListener(null); // refresh it make sure it doens't stop workin
+//        holder.rsvpToggle.setChecked(hangout.getParticipants().contains(me)); // check is it in there first?
+//        holder.rsvpToggle.setEnabled(!hangout.isPast()); // disable if past event
+        holder.rsvpToggle.setOnCheckedChangeListener(((buttonView, isChecked) -> { // do the stuff
+            if(isChecked){
+                FirebaseFirestore.getInstance().collection("hangouts").document(hangout.getId()).update("participants", FieldValue.arrayUnion(me)).addOnSuccessListener(unused -> {
+//                    hangout.addParticipant(me);
+                    Toast.makeText(context, "RSVP Added", Toast.LENGTH_SHORT).show();
+                    notifyDataSetChanged();
+                });
+            }
+            else{
+                FirebaseFirestore.getInstance().collection("hangouts").document(hangout.getId()).update("participants", FieldValue.arrayRemove(me)).addOnSuccessListener(unused -> {
+//                    hangout.removeParticipant(me);
+                    Toast.makeText(context, "RSVP Removed", Toast.LENGTH_SHORT).show();
+                    notifyDataSetChanged();
+                });
+            }
+        }));
         // Set name (with group name if showing all)
         if (showGroupName && hangout.getGroupId() != null) {
             String groupName = groupIdToName.get(hangout.getGroupId());
@@ -86,11 +111,14 @@ public class HangoutAdapter extends RecyclerView.Adapter<HangoutAdapter.HangoutV
         TextView dateTextView;
         TextView locationTextView;
 
+        Switch rsvpToggle;
+
         HangoutViewHolder(View itemView) {
             super(itemView);
             nameTextView = itemView.findViewById(R.id.hangout_name);
             dateTextView = itemView.findViewById(R.id.hangout_date);
             locationTextView = itemView.findViewById(R.id.hangout_location);
+            rsvpToggle = itemView.findViewById(R.id.rsvpSwitch);
         }
     }
 }
